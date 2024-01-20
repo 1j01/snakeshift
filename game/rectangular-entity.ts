7c36f32145c4c550d43927d500ee8b685ee49206
Isaiah Odhner <isaiahodhner@gmail.com>
2024-01-20 08:45:12 -0500

    Note a case where there are still seams

--- game/rectangular-entity.ts
+++ game/rectangular-entity.ts
@@ -26,9 +26,17 @@ export class RectangularEntity extends Entity {
     // and black is zero, so adding it doesn't change anything.
     // This blend mode can be used to blend arbitrary colors seamlessly, but not if they overlap.
     // https://stackoverflow.com/a/53292886/2624876
+
     // A more general approach is to extend the coordinates.
+
     // (A stroke works similarly, but is imperfect, as it has its own anti-aliasing.
     // If you use the fill tool in an image editor, you can see that it leaves a slightly different color at the edges.)
+
+    // Offsetting by a single pixel is almost perfect. The change in geometry is usually not noticeable.
+    // However there's a case where there's still a seam: if two blocks overlap, one white, one black,
+    // sharing one or more edges, those edges will be offset (or not), in this scheme, the same way as each other,
+    // rather than one extending to cover the other.
+
     const pixel = 1 / ctx.getTransform().a
     ctx.fillStyle = this.layer === CollisionLayer.White ? '#fff' : '#000'
     ctx.fillRect(this.x, this.y, this.width + pixel, this.height + pixel)
