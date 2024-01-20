77b848e52ef6f6d11b625dd77ccdb3aa12dc8a47
Isaiah Odhner <isaiahodhner@gmail.com>
2024-01-19 23:50:39 -0500

    Draw outline for collectables on their own color

--- game/collectable.ts
+++ game/collectable.ts
@@ -25,12 +25,17 @@ export class Collectable extends Entity {
 
   draw(ctx: CanvasRenderingContext2D) {
     ctx.save()
+    ctx.lineWidth = 0.1
+    ctx.strokeStyle = this.layer === CollisionLayer.White ? '#000' : '#fff'
     ctx.fillStyle = this.layer === CollisionLayer.White ? '#fff' : '#000'
     ctx.translate(this.x, this.y)
     ctx.scale(this.size, this.size)
     ctx.translate(1 / 2, 1 / 2)
     ctx.rotate(Math.PI / 4)
-    ctx.fillRect(-Collectable.SIZE / 2, -Collectable.SIZE / 2, Collectable.SIZE, Collectable.SIZE)
+    ctx.beginPath()
+    ctx.rect(-Collectable.SIZE / 2, -Collectable.SIZE / 2, Collectable.SIZE, Collectable.SIZE)
+    ctx.stroke()
+    ctx.fill()
     ctx.restore()
   }
 }
