b91ce6007d5d8d129adc9a0312e654668c1ce5bd
Isaiah Odhner <isaiahodhner@gmail.com>
2024-01-20 04:54:59 -0500

    Avoid seams on white walls

--- game/rectangular-entity.ts
+++ game/rectangular-entity.ts
@@ -19,9 +19,16 @@ export class RectangularEntity extends Entity {
     return CollisionLayer.None
   }
 
-  // used as-is by Block, but here as a placeholder for new entities
+  // used as-is by Block, but defined here as a placeholder for new entities as well
   draw(ctx: CanvasRenderingContext2D) {
+    // "lighter" avoids seams between adjacent blocks
+    // There is no equivalent for the color black, as this works only by using additive blending instead of alpha blending,
+    // and black is zero, so adding it doesn't change anything.
+    // This blend mode can be used to blend arbitrary colors seamlessly, but not if they overlap.
+    // https://stackoverflow.com/a/53292886/2624876
+    ctx.globalCompositeOperation = this.layer === CollisionLayer.White ? 'lighter' : 'source-over'
     ctx.fillStyle = this.layer === CollisionLayer.White ? '#fff' : '#000'
     ctx.fillRect(this.x, this.y, this.width, this.height)
+    ctx.globalCompositeOperation = 'source-over'
   }
 }
