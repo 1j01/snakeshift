644b4b761dad10cea9e0612a30333053d3cb7916
Isaiah Odhner <isaiahodhner@gmail.com>
2024-01-23 17:11:25 -0500

    Unify Bresenham code
    I don't like to imply things are more different than they are.
    
    (I've chosen the longer version here, but not because I like it better.)

--- game/helpers.ts
+++ game/helpers.ts
@@ -19,19 +19,6 @@ export function neighborOf(tile: Tile, direction: { x: number, y: number }) {
   }
 }
 
-// int dx =  abs(x1-x0), sx = x0<x1 ? 1 : -1;
-// int dy = -abs(y1-y0), sy = y0<y1 ? 1 : -1; 
-// int err = dx+dy, e2; /* error value e_xy */
-
-// for(;;){  /* loop */
-//    setPixel(x0,y0);
-//    if (x0==x1 && y0==y1) break;
-//    e2 = 2*err;
-//    if (e2 >= dy) { err += dy; x0 += sx; } /* e_xy+e_x > 0 */
-//    if (e2 <= dx) { err += dx; y0 += sy; } /* e_xy+e_y < 0 */
-// }
-
-
 export function* bresenham(start: Point, end: Point): Generator<Point> {
   const xDist = Math.abs(end.x - start.x)
   const yDist = -Math.abs(end.y - start.y)
@@ -74,9 +61,13 @@ export function* lineNoDiagonals(start: Point, end: Point): Generator<Point> {
   let y = start.y
   let error = xDist + yDist
 
-  yield { x, y }
 
-  while (x != end.x || y != end.y) {
+  while (true) {
+    yield { x, y }
+
+    if (x === end.x && y === end.y) {
+      break
+    }
 
     // if (xDist + yDist < 4 * error) { // alternate form, probably less clear but infinitesimally faster
     if (2 * error - yDist > xDist - 2 * error) {
@@ -88,8 +79,6 @@ export function* lineNoDiagonals(start: Point, end: Point): Generator<Point> {
       error += xDist
       y += yStep
     }
-
-    yield { x, y }
   }
 }
 
