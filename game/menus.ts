ebced210e876f241ce6a1b9aa6e186b7b9a3e456
Isaiah Odhner <isaiahodhner@gmail.com>
2024-02-06 22:45:26 -0500

    Navigate main menu with arrow keys

--- game/menus.ts
+++ game/menus.ts
@@ -8,9 +8,9 @@ const levelEditorButton = document.querySelector('#level-editor-button')!
 const creditsButton = document.querySelector('#credits-button')!
 const backButtons = document.querySelectorAll('.back-to-main-menu-button')!
 
-const mainMenu = document.querySelector('#main-menu')!
-const levelSelect = document.querySelector('#level-select')!
-const credits = document.querySelector('#credits')!
+const mainMenu = document.querySelector<HTMLDivElement>('#main-menu')!
+const levelSelect = document.querySelector<HTMLDivElement>('#level-select')!
+const credits = document.querySelector<HTMLDivElement>('#credits')!
 
 export function initMainMenu() {
   playButton.addEventListener('click', () => {
@@ -39,6 +39,19 @@ export function initMainMenu() {
   for (const backButton of backButtons) {
     backButton.addEventListener('click', showMainMenu)
   }
+
+  addEventListener("keydown", (event) => {
+    if (mainMenu.classList.contains("active")) {
+      if (event.key === "ArrowDown" || event.key === "ArrowUp") {
+        event.preventDefault()
+        const buttons = mainMenu.querySelectorAll("button")
+        const index = Array.from(buttons).indexOf(document.activeElement as HTMLButtonElement)
+        const delta = event.key === "ArrowDown" ? 1 : -1
+        const nextIndex = index === -1 ? 0 : (index + delta + buttons.length) % buttons.length
+        buttons[nextIndex].focus()
+      }
+    }
+  })
 
   showMainMenu()
 }
