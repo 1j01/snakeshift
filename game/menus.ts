24f8e2f032bd7297996fab99a56cbc9f4240d08f
Isaiah Odhner <isaiahodhner@gmail.com>
2024-02-06 21:29:02 -0500

    Add back buttons to return to the main menu

--- game/menus.ts
+++ game/menus.ts
@@ -2,16 +2,17 @@ import { setActivityMode } from "./game"
 import { clearLevel } from "./game-state"
 import { loadFirstLevel } from "./level-select"
 
+const playButton = document.querySelector('#play-button')!
+const levelSelectButton = document.querySelector('#level-select-button')!
+const levelEditorButton = document.querySelector('#level-editor-button')!
+const creditsButton = document.querySelector('#credits-button')!
+const backButtons = document.querySelectorAll('.back-to-main-menu-button')!
+
+const mainMenu = document.querySelector('#main-menu')!
+const levelSelect = document.querySelector('#level-select')!
+const credits = document.querySelector('#credits')!
+
 export function initMainMenu() {
-  const playButton = document.querySelector('#play-button')!
-  const levelSelectButton = document.querySelector('#level-select-button')!
-  const levelEditorButton = document.querySelector('#level-editor-button')!
-  const creditsButton = document.querySelector('#credits-button')!
-
-  const mainMenu = document.querySelector('#main-menu')!
-  const levelSelect = document.querySelector('#level-select')!
-  const credits = document.querySelector('#credits')!
-
   playButton.addEventListener('click', () => {
     hideScreens()
     loadFirstLevel()
@@ -37,10 +38,19 @@ export function initMainMenu() {
 
   mainMenu.classList.add('active')
 
+  for (const backButton of backButtons) {
+    backButton.addEventListener('click', showMainMenu)
+  }
 }
 
 export function hideScreens() {
   for (const screen of document.querySelectorAll('.screen.active')) {
     screen.classList.remove('active')
   }
+}
+
+export function showMainMenu() {
+  hideScreens()
+  mainMenu.classList.add('active')
+  setActivityMode("menu")
 }
