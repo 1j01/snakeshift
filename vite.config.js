93f3096f2fd35464e845c6a6ce3389d499cda218
Isaiah Odhner <isaiahodhner@gmail.com>
2024-02-07 14:38:10 -0500

    Add @vitejs/plugin-legacy

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import legacy from '@vitejs/plugin-legacy'
 
 export default defineConfig({
   root: 'game',
@@ -12,4 +13,11 @@ export default defineConfig({
   // Without this, you don't get errors when files fail to load!
   // It'll "successfully" return your index.html when trying to load .css or anything.
   appType: 'mpa',
+
+  // Toying around with the idea of supporting old Kindle experimental browser.
+  plugins: [
+    legacy({
+      targets: ['defaults'],
+    }),
+  ],
 })
