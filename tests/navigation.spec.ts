a5c235609ca2b12e6ea62d0b6a4765e6de3bee50
Isaiah Odhner <isaiahodhner@gmail.com>
2024-08-09 17:04:13 -0400

    Pick a port
    It's 55 = SS = SnakeShift + 69 = yin yang :)
    
    nice, right?

--- tests/navigation.spec.ts
+++ tests/navigation.spec.ts
@@ -1,7 +1,7 @@
 import { expect, test } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
-  await page.goto('http://localhost:5173/');
+  await page.goto('http://localhost:5569/');
 });
 
 test('has title', async ({ page }) => {
@@ -38,5 +38,5 @@ test('test', async ({ page }) => {
     }
   });
   await page.getByRole('button', { name: 'Snake (White)' }).press('ControlOrMeta+z');
-  await page.goto('http://localhost:5173/');
+  await page.goto('http://localhost:5569/');
 });
