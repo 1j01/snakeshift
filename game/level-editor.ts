1j01/snakeshift

commit 8a94609622efd91790c253cdb8b508637101f165

Allow dragging an empty selection, for consistency

game/level-editor.ts
@@ -345,7 +345,7 @@ export function handleInputForLevelEditing(
       mouseHoveredTile = undefined
     }
     if (mouseHoveredTile) {
-      if (draggingEntities.length) {
+      if (dragGestureLastTile) {
         if (selectionRange && dragGestureLastTile) {
           const dx = mouseHoveredTile.x - dragGestureLastTile.x
           const dy = mouseHoveredTile.y - dragGestureLastTile.y

commit f628b5267f348c7b6626f2ef749534ef50c03ed8

Fix move tool
(broken in 8a94609622efd91790c253cdb8b508637101f165 "Allow dragging an empty selection, for consistency")

game/level-editor.ts
@@ -345,7 +345,12 @@ export function handleInputForLevelEditing(
       mouseHoveredTile = undefined
     }
     if (mouseHoveredTile) {
-      if (dragGestureLastTile) {
+      if (
+        // For move tool
+        draggingEntities.length ||
+        // For select tool, allow dragging even empty selection.
+        dragGestureLastTile
+      ) {
         if (selectionRange && dragGestureLastTile) {
           translateSelection(mouseHoveredTile.x - dragGestureLastTile.x, mouseHoveredTile.y - dragGestureLastTile.y)
           dragGestureLastTile = mouseHoveredTile