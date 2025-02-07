dcf3147801dfe115d160958898d00d207015f3ac
Isaiah Odhner <isaiahodhner@gmail.com>
2025-02-07 13:49:53 -0500

    Shorten move list text representation

--- tests/gameplay.spec.ts
+++ tests/gameplay.spec.ts
@@ -76,7 +76,7 @@ test('game should be beatable (using recorded playthroughs)', async ({ page }) =
       continue
     }
     const moves = getMovesFromPlaythrough(playthroughJSON)
-    console.log(`Playing level ${levelId} with ${moves.length} moves: ${moves.map((move) => typeof move === 'object' ? `click (${move.click.x}, ${move.click.y})` : move).join(', ')}`)
+    console.log(`Playing level ${levelId} with ${moves.length} moves: ${moves.map((move) => typeof move === 'object' ? `click(${move.click.x},${move.click.y})` : { "ArrowUp": "↑", "ArrowDown": "↓", "ArrowLeft": "←", "ArrowRight": "→" }[move]).join(' ')}`)
     for (const move of moves) {
       if (move) {
         if (typeof move === 'object' && 'click' in move) {
