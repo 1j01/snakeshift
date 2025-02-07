82521cff04f466cf90cf177bd7b540dea1d933f5
Isaiah Odhner <isaiahodhner@gmail.com>
2025-02-06 21:07:22 -0500

    Add todo comments to make functions non-empty

--- tests/gameplay.spec.ts
+++ tests/gameplay.spec.ts
@@ -22,19 +22,19 @@ declare global {
   }
 }
 
-test.skip('snake should not move past level boundaries', () => { })
-test.skip('snake should not move through walls', () => { })
-test.skip('snake should not move through itself', () => { })
-test.skip('snake should be able to move to its tail location, since its tail will move', () => { })
-test.skip('snake should not move through other snakes of the same color', () => { })
-test.skip('snake should not move if another snake is on top', () => { })
+test.skip('snake should not move past level boundaries', () => { /* TODO */ })
+test.skip('snake should not move through walls', () => { /* TODO */ })
+test.skip('snake should not move through itself', () => { /* TODO */ })
+test.skip('snake should be able to move to its tail location, since its tail will move', () => { /* TODO */ })
+test.skip('snake should not move through other snakes of the same color', () => { /* TODO */ })
+test.skip('snake should not move if another snake is on top', () => { /* TODO */ })
 // test.skip('if we WERE supporting snakes moving while another snake is on top, the snake should not be able to swap depths with the above snake by moving onto it', ()=>{ });
 // test.skip('if we WERE supporting snakes moving while another snake is on top, the snake should not be able to move out from under the other snake, since it would be providing the ground for it', ()=>{ });
 // test.skip('if we WERE supporting snakes moving while another snake is on top, the snake should be able to move out from under the other snake if its tail will fill the gap immediately', ()=>{ });
-test.skip('should detect immobile state and show a message about restarting/undoing', () => { })
-test.skip('extra undo states should be skipped or merged when switching snakes multiple times', () => { })
-test.skip('gamepad controls should be supported', () => { })
-test.skip('touch controls should be supported', () => { })
+test.skip('should detect immobile state and show a message about restarting/undoing', () => { /* TODO */ })
+test.skip('extra undo states should be skipped or merged when switching snakes multiple times', () => { /* TODO */ })
+test.skip('gamepad controls should be supported', () => { /* TODO */ })
+test.skip('touch controls should be supported', () => { /* TODO */ })
 
 
 type Move = 'ArrowUp' | 'ArrowDown' | 'ArrowLeft' | 'ArrowRight' | { click: Tile };
