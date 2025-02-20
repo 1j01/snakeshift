86e7043a0a68e296f07a801548377837d3e3b992
Isaiah Odhner <isaiahodhner@gmail.com>
2025-02-19 19:20:16 -0500

    Test round trip re-saving of playthrough files

---
+++ tests/format.spec.ts
@@ -0,0 +1,19 @@
+import { expect, test } from '@playwright/test'
+import { readFile } from 'fs/promises'
+import { getPlaythroughContent, setLevelContent } from './test-helpers'
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('http://localhost:5569/?show-test-levels')
+
+  // Fail test on any page error
+  page.on('pageerror', (error) => {
+    throw new Error(`Uncaught exception: ${error.stack}`)
+  })
+})
+
+test('round-trip: should re-save playthrough file identically', async ({ page }) => {
+  const originalContent = await readFile('game/public/levels/tests/overlapped-snake-doubling-back-bug-replay.json', 'utf8')
+  await setLevelContent(page, originalContent, "replay")
+  const newContent = await getPlaythroughContent(page)
+  expect(newContent).toEqual(originalContent)
+})
