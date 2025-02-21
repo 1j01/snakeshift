14cd8e7d1c9a115498b4a0cfc6e858cca94a9fb9
Isaiah Odhner <isaiahodhner@gmail.com>
2025-02-20 19:50:26 -0500

    Tweak round-trip test, note flakiness

--- tests/format.spec.ts
+++ tests/format.spec.ts
@@ -12,15 +12,21 @@ test.beforeEach(async ({ page }) => {
 })
 
 test('round-trip: should re-save playthrough file identically', async ({ page }) => {
+  // FIXME: this is flaking, especially when running from a cold state (no browser opened, running this test specifically)
+  // It seems to be caused by the level select screen rendering level previews
+  // Of course, when introducing the level previews, I made sure to try to synchronously restore the level content
+  // after the level preview is rendered, to a snapshot taken synchronously immediately before changing the level content to render the preview
+  // (NOT before loading the level file), but it was still a hack, and I knew it could cause problems like this.
+  // When this test flakes, the baseState is empty and the next delta includes the entire level state. (Not sure if it's the first or second state. Doesn't matter.)
   const originalContent = await readFile('game/public/levels/tests/overlapped-snake-doubling-back-bug-replay.json', 'utf8')
   await setLevelContent(page, originalContent, "replay")
   const newContent = await getPlaythroughContent(page)
-  expect(newContent).toEqual(originalContent)
+  expect(JSON.parse(newContent)).toEqual(JSON.parse(originalContent))
 
   // Load a second file to ensure they're not concatenated, which was a previous bug
   // This test should fail if you revert commit 101ea449cb30fddab122d8067e070c00fff9b1a2
   const originalContent2 = await readFile('game/public/levels/tests/overlapped-snake-looping-back-bug-replay.json', 'utf8')
   await setLevelContent(page, originalContent2, "replay")
   const newContent2 = await getPlaythroughContent(page)
-  expect(newContent2).toEqual(originalContent2)
+  expect(JSON.parse(newContent2)).toEqual(JSON.parse(originalContent2))
 })
