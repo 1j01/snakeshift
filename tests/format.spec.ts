43527085fcc24691ed079cd15ef10f20bd3064ce
Isaiah Odhner <isaiahodhner@gmail.com>
2025-02-28 00:24:00 -0500

    Add round-trip test for level files

--- tests/format.spec.ts
+++ tests/format.spec.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@playwright/test'
 import { readFile } from 'fs/promises'
-import { getPlaythroughContent, setLevelContent } from './test-helpers'
+import { getCurrentLevelContent, getPlaythroughContent, setLevelContent } from './test-helpers'
 
 test.beforeEach(async ({ page }) => {
   await page.goto('http://localhost:5569/?show-test-levels')
@@ -22,5 +22,17 @@ test('round-trip: should re-save playthrough file identically', async ({ page })
   const originalContent2 = await readFile('game/public/levels/tests/overlapped-snake-looping-back-bug-replay.json', 'utf8')
   await setLevelContent(page, originalContent2, "replay")
   const newContent2 = await getPlaythroughContent(page)
+  expect(JSON.parse(newContent2)).toEqual(JSON.parse(originalContent2))
+})
+
+test('round-trip: should re-save level file identically', async ({ page }) => {
+  const originalContent = await readFile('game/public/levels/tests/3x3-with-1x1-snake-in-middle.json', 'utf8')
+  await setLevelContent(page, originalContent, "edit")
+  const newContent = await getCurrentLevelContent(page)
+  expect(JSON.parse(newContent)).toEqual(JSON.parse(originalContent))
+
+  const originalContent2 = await readFile('game/public/levels/tests/snake-should-be-able-to-move-down-to-its-tail.json', 'utf8')
+  await setLevelContent(page, originalContent2, "edit")
+  const newContent2 = await getCurrentLevelContent(page)
   expect(JSON.parse(newContent2)).toEqual(JSON.parse(originalContent2))
 })
