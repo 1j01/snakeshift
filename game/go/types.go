cc8550d9fbcb0b8824991ef03f3f1cf2bfcce92e
Isaiah Odhner <isaiahodhner@gmail.com>
2025-07-04 15:59:58 -0400

    WIP

---
+++ game/go/types.go
@@ -0,0 +1,45 @@
+package main
+
+type CollisionLayer int
+
+const (
+	Neither CollisionLayer = 0
+	White   CollisionLayer = 1
+	Black   CollisionLayer = 2
+	Both    CollisionLayer = 3 // = White | Black
+)
+
+type Point struct {
+	X int `json:"x"`
+	Y int `json:"y"`
+}
+
+type Food struct {
+	Position Point
+	Layer    CollisionLayer
+}
+
+type Snake struct {
+	ID             int
+	Segments       []Point // ordered from head to tail
+	GrowOnNextMove bool
+}
+
+type LevelInfo struct {
+	Width  int `json:"width"`
+	Height int `json:"height"`
+}
+
+// Custom marshaling is defined for the top-level struct.
+type Level struct {
+	Info   LevelInfo
+	Grid   [][]CollisionLayer
+	Foods  []Food
+	Snakes []Snake
+}
+
+type Hit struct {
+	entity       Snake
+	segmentIndex int
+	layer        CollisionLayer
+}
