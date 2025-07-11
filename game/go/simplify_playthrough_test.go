5bc8e422963771094fad073e227569a5320d292a
Isaiah Odhner <isaiahodhner@gmail.com>
2025-07-11 11:31:42 -0400

    Add table of states/moves

--- game/go/simplify_playthrough_test.go
+++ game/go/simplify_playthrough_test.go
@@ -12,6 +12,19 @@ func TestSimplifyPlaythrough(t *testing.T) {
 	}
 
 	snakeId := "04f13bbb-2635-4470-9849-eaaecc079201"
+
+	// | state index | move index | state    | direction     |
+	// | ----------- | ---------- | -------- | ------------- |
+	// | 0           |            | ( 0,  0) | initial state |
+	// | 1           | 0          | ( 0, -1) | Up            |
+	// | 2           | 1          | (-1, -1) | Left          |
+	// | 3           | 2          | (-2, -1) | Left          |
+	// | 4           | 3          | (-3, -1) | Left          |
+	// | 5           | 4          | (-3,  0) | Down          |
+	// | 6           | 5          | (-2,  0) | Right         |
+	// | 7           | 6          | (-1,  0) | Right         |
+	// | 8           | 7          | ( 0,  0) | Right         |
+	// | 9           | 8          | ( 1,  0) | Right         |
 	moveInputs := []MoveInput{
 		{Direction: Up, SnakeID: snakeId},
 		{Direction: Left, SnakeID: snakeId},
